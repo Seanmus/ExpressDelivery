5c807339ce3e9726f53f2a3663e82f8c0f89ca75
BagelDesu <36751805+BagelDesu@users.noreply.github.com>
2023-04-30 12:36:13 -0700

    Added Resupply & Delivery Checkpoints

---
+++ Assets/Scripts/Environment/Checkpoints/ResupplyPoint.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResupplyPoint : MonoBehaviour
+{
+    [SerializeField]
+    private string resourceName;
+
+    [SerializeField]
+    private float amount;
+
+    public void Resupply()
+    {
+        Inventory.Instance.AddItem(resourceName, amount);
+        Destroy(gameObject);
+    }
+}
