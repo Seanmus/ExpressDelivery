be80316d4c954975ca03ef9e207e1459db6e977b
BagelDesu <36751805+BagelDesu@users.noreply.github.com>
2023-04-30 19:46:36 -0700

    Added Intro Section to Map

--- Assets/Scripts/Systems/Objective/DeliveryPoint.cs
+++ Assets/Scripts/Systems/Objective/DeliveryPoint.cs
@@ -6,6 +6,9 @@ public class DeliveryPoint : MonoBehaviour
 {
     public bool isObjectiveFinished {get; private set;} = false;
 
+    [SerializeField]
+    private int timerIncrease = 5;
+
     private void Start()
     {
         BagelDesu.ObjectiveManager.Instance.RegisterPoint(this);
@@ -16,6 +19,7 @@ public class DeliveryPoint : MonoBehaviour
         if (isObjectiveFinished == false)
         {
             isObjectiveFinished = true;
+            Timer.Instance.UpdateTime(timerIncrease);
             BagelDesu.ObjectiveManager.Instance.DeliverPackage();
         }
     }
