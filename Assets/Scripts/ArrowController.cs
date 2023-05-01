d3d78337cb500f356e17e907fd6cd8da96aef69b
Seanmus <seanpiche718@gmail.com>
2023-04-30 21:55:59 -0500

    arrow points to closest objective

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class ArrowController : MonoBehaviour
 {
     public Transform target;
+    private DeliveryPoint closestDeliveryPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        closestDeliveryPoint = BagelDesu.ObjectiveManager.Instance.packages[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target, Vector3.right);
+        if(BagelDesu.ObjectiveManager.Instance.packages.Count > 0)
+        {
+
+            foreach (DeliveryPoint deliveryPoint in BagelDesu.ObjectiveManager.Instance.packages)
+            {
+                if (deliveryPoint.isObjectiveFinished)
+                {
+                    continue;
+                }
+                if (Vector3.Distance(deliveryPoint.transform.position, this.transform.position) < 
+                    Vector3.Distance(closestDeliveryPoint.transform.position, this.transform.position)|| closestDeliveryPoint.isObjectiveFinished)
+                {
+                    closestDeliveryPoint = deliveryPoint;
+                }
+            }
+            transform.LookAt(closestDeliveryPoint.transform, Vector3.right);
+        }
     }
 }
